6fd8b276873fa8a94a2d8e7e07f06a63c8f61f7b
miloszkuszczak <m.kuszczak86@gmail.com>
2019-07-08 16:44:38 +0200

    change component structure

--- dev/js/app.js
+++ dev/js/app.js
@@ -11,12 +11,19 @@ import {
 
 
 class Cites extends Component {
-
+  constructor(props) {
+    super(props);
+    
+  }
   render() {
+    const citeNo = this.props.citeNo
+
     const cites = ['"Kupuj kiedy leje się krew"', '"Hossa wspina sie po ścianie strachu"', '"Bój się kiedy inni sa chytrzy"', '"Trend is your friend"'];
   
-  return <h1>{cites[Math.floor(Math.random()*cites.length)]}</h1>
-  }
+  return (<>
+          <h1>{cites[citeNo]}</h1>
+          </>
+  )}
 }
 
 class StockSearch extends Component {
@@ -24,7 +31,6 @@ class StockSearch extends Component {
     super(props);
       this.state ={
       stock: '',
-      changeSite: false,
     }
   }
   handleChange(e) {
@@ -49,31 +55,31 @@ class StockSearch extends Component {
 class Main extends Component {
   render() {
     return (<>
-            <div class="container">
-   <div class="row">
+            <div className="container">
+   <div className="row">
         <div class="col-xs-12">
            <header>To będzie info o stronie/wybranej spółce     || Kurs Dnia    || Inne informacje</header>
         </div>
-        <div class="col-xs-3">
+        <div className="col-xs-3">
             <div class="element">To jest element z ważnymi wydarzeniami w danym roku</div>
         </div>
 
-        <div class="col-xs-6">
+        <div className="col-xs-6">
            <section class="element">Tu wykres (skorzystam z bilbiotek wykresu za dany rok)</section>
          </div>
-         <div class="col-xs-3">
-               <div class="element"><div>Dywidendy</div></div>
+         <div className="col-xs-3">
+               <div className="element"><div>Dywidendy</div></div>
       </div>
-      <div class="col-xs-6">
-          <div class="element"><div>Rekomendacje</div></div>
+      <div className="col-xs-6">
+          <div className="element"><div>Rekomendacje</div></div>
         </div>
-        <div class="col-xs-6">
-            <section class="element">Dziennik inwestora [textarea=>local Storage</section>
+        <div className="col-xs-6">
+            <section className="element">Dziennik inwestora [textarea=>local Storage</section>
         </div>
 
 
-        <div class="col-xs-12">
-           <footer><span class="line"><div class="circle">2019</div></span></footer>
+        <div className="col-xs-12">
+           <footer><span className="line"><div class="circle">2019</div></span></footer>
        </div>
     </div>
 
@@ -94,12 +100,51 @@ class FirstView extends Component {
   }
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      changeSite: false,
+      citeNo: 0,
+    }
+  }
+
+componentDidMount() {
+  this.setState({
+    citeNo: getRandomIntInclusive(0,3),
+  })
+}
+  handleChange(e) {
+    this.setState({
+        [e.target.name]: e.target.value
+})
+}
+  handleEnter(e) {
+    if (e.keyCode == 13) {
+        this.setState({
+          changeSite: true,
+        })
+    }
+  }
   
-  
+
+
   render() {
-    return ( <FirstView />
-    )
+    if (this.state.changeSite == false) {
+    return ( <>
+              <Cites citeNo={this.state.citeNo}/>
+              <input type='text' name="stock" value={this.state.stock} onChange={e=>this.handleChange(e)} onKeyDown={e=>this.handleEnter(e)}/>
+            </>
+    )}
+    else {
+      return <Main/> 
+    }
   }
 }
 
