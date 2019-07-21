95ab12a99743f59ca6863bca558b953f07c43827
miloszkuszczak <m.kuszczak86@gmail.com>
2019-07-21 18:52:57 +0200

    adding features

--- dev/js/app.js
+++ dev/js/app.js
@@ -9,26 +9,20 @@ import {
   NavLink,
 } from 'react-router-dom';
 import Select from 'react-select';
+import { Lines } from 'react-preloaders';
 import { Dividends, LineGraph, Shares, Results, FinFactor, StockInfo } from './components/stock.js';
+
 
 
 class Cites extends Component {
   constructor(props) {
     super(props);
-
   }
   render() {
-    const citeNo = this.props.citeNo
-    const aaa = [
-      { author: "Warren Buffet", cites: ["Na giełdzie kapitał płynie od aktywnych do cierpliwych", "Wall Street to miejsce, gdzie ludzie przyjeżdżający limuzynami proszą o rady tych przyjeżdżających metrem",] },
-      { author: "", cites: ["Rynek potrafi być dłużej irracjonalny niż inwestor wypłacalny", "Trend is your friend"] }]
-    const cites = ['"Kupuj kiedy leje się krew"', '"Hossa wspina sie po ścianie strachu"', '"Bój się kiedy inni sa chytrzy"', '"Trend is your friend"'];
-    return (<>
-      <h1>{cites[citeNo]}</h1>
-    </>
-    )
+    return (<><h2><cite>{this.props.cite}</cite></h2> <h3>{this.props.author}</h3></>)
   }
 }
+
 
 
 class Header extends Component {
@@ -36,7 +30,12 @@ class Header extends Component {
     super(props);
   }
   render() {
-    return (<>Nazwa spółki: {this.props.stock.name}, {this.props.stock.ticker}, Cena debiutu: {this.props.stock.price_IPO}, Data debiutu: {this.props.stock.date_IPO}, Wyszukaj nową spółkę:</>)
+    return (<>
+      <h1>{this.props.stock.name}</h1>
+      <h2>Ticker: {this.props.stock.ticker}</h2>
+      <h3>Cena debiutu: {this.props.stock.price_IPO}</h3>
+      <h3>Data debiutu: {this.props.stock.date_IPO}</h3>
+      <Search options={this.props.options} handler={this.props.handler} /></>)
   }
 
 }
@@ -53,44 +52,46 @@ class Main extends Component {
     }
   }
 
+  componentWillReceiveProps(newProps) {
+    debugger;
+    this.setState({
+      selectedYear: this.GetYearsToShow(newProps.stockData[0].years)[0],
+    })
+  }
+
   GetYearsToShow(years) {
     const yearsToShow = years.map(item => { return item.year }).sort((a, b) => { a > b });
     return yearsToShow;
   }
 
   componentDidMount() {
-
+    debugger;
     let yearsToShow = this.GetYearsToShow(this.state.stockData.years);
     let selectedYear = yearsToShow[0];
     this.setState({ yearsToShow, selectedYear });
   }
 
+
   handleClick(e) {
     this.setState({
       selectedYear: e.target.value,
-
     })
   }
 
 
   render() {
     debugger;
-
-    const actualYearData = this.state.stockData.years.filter((elem) => { return elem.year == this.state.selectedYear });
-
-    const allDataFromStartToToday = this.state.stockData.years.filter((elem) => { return elem.year <= this.state.selectedYear });
-
-    const previousYearData = this.state.stockData.years.filter(elem => { return elem.year == this.state.selectedYear - 1 });
-
+    const yearsToShow = this.GetYearsToShow(this.props.stockData[0].years);
+    const actualYear = this.state.selectedYear;
+    const actualYearData = this.props.stockData[0].years.filter((elem) => { return elem.year == actualYear });
+    const allDataFromStartToToday = this.props.stockData[0].years.filter((elem) => { return elem.year <= actualYear });
+    const previousYearData = this.props.stockData[0].years.filter(elem => { return elem.year == (actualYear - 1) });
     const dividendsFromIPO = allDataFromStartToToday.map(item => parseFloat(item.dividend)).reduce((a, b) => (a + b));
-
     const shares = actualYearData[0].shareholders.map(x => [x.share]);
-
     const holders = actualYearData[0].shareholders.map(x => [x.holder]);
-
     const info = actualYearData[0].events.map(x => [x]);
 
-    return (<><div className="stick-header"><header><Header stock={this.state.stockData} /></header></div>
+    return (<>
       <div className="container">
         <div className="row">
           <div className="col-xs-12">
@@ -110,11 +111,11 @@ class Main extends Component {
           </div>
           <div className="col-xs-3">
             <div className="element">
-              <Dividends dividend={dividendsFromIPO} price={this.state.stockData.price_IPO} actualYear={actualYearData[0]} />
+              <Dividends dividend={dividendsFromIPO} price={this.props.stockData[0].price_IPO} actualYear={actualYearData[0]} />
             </div>
 
           </div>
-          <div className="col-xs-4">
+          <div className="col-xs-4 col-m-12">
             <div className="element">
               <div><FinFactor actualYear={actualYearData[0]} previousYear={previousYearData.length !== 0 ? previousYearData[0] : actualYearData[0]} /></div>
             </div>
@@ -130,13 +131,12 @@ class Main extends Component {
           </div>
         </div>
       </div>
-      <div className="stick-footer"><footer><div className="navigation"><span className='line'>{this.state.yearsToShow.length > 0 ? this.state.yearsToShow.map(year => <button key={year} value={year} onClick={e => this.handleClick(e)} className="circle">{year}</button>) : ""}</span></div></footer>
+      <div className="stick-footer"><footer><div className="navigation"><span className='line'>{yearsToShow.length > 0 ? yearsToShow.map(year => <button key={year} value={year} onClick={e => this.handleClick(e)} className="circle">{year}</button>) : ""}</span></div></footer>
       </div>
     </>
     )
   }
 }
-
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(0);
@@ -158,12 +158,39 @@ class App extends Component {
 }
 
 
-class LandingPage extends Component {
+class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedCompany: '',
+      clearable: true,
+      searchable: true,
+    }
+  }
+  render() {
+    return <Select
+      className='selector inHeader'
+      name="companyNameSelector"
+      placeholder='Wybierz spółkę'
+      value=''
+      onChange={this.props.handler}
+      clearable={this.state.clearable}
+      searchable={this.state.searchable}
+      options={this.props.options}
+    />
+  }
+}
+
+
+
+class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.handler = this.handler.bind(this)
+    this.state = {
       changeSite: false,
-      citeNo: 0,
+      author: '',
+      cite: '',
       stockData: '',
       selectedStockData: '',
       selectedCompany: '',
@@ -173,52 +200,102 @@ class LandingPage extends Component {
     }
   }
 
+  handler(searchCompany) {
+    sessionStorage.setItem("sessionSelectedCompany", searchCompany.value);
+    let selectedStockData = "";
+    if (searchCompany.value) {
+      selectedStockData = this.state.stockData.stocks.filter(x => x.name === searchCompany.value);
+    }
+    this.setState({
+      selectedCompany: searchCompany.value,
+      selectedStockData: selectedStockData ? selectedStockData : "",
 
+    })
+  }
+
+
+  randomCite() {
+    const cite = [
+      { author: "Warren Buffet", cites: ["Na giełdzie kapitał płynie od aktywnych do cierpliwych", "Wall Street to miejsce, gdzie ludzie przyjeżdżający limuzynami proszą o rady tych przyjeżdżających metrem",] },
+      { author: "dr Alexander Elder", cites: ["Rynek potrafi być dłużej irracjonalny niż inwestor wypłacalny", "Trend is your friend"] },
+      { author: "George Soros", cites: ["Bitcoin jest super", "Lubię oszukiwać ludzi na spekulacji"] }];
+
+    const citesArray = cite.map(element => [element.author, element.cites]);
+    const authorNo = getRandomIntInclusive(0, citesArray.length - 1);
+    const exactCite = citesArray[authorNo][1].map(element => element);
+    return ({
+      author: citesArray[authorNo][0],
+      cite: exactCite[getRandomIntInclusive(0, exactCite.length - 1)],
+    });
+  }
 
   componentDidMount() {
-    this.setState({
-      citeNo: getRandomIntInclusive(0, 3),
-    })
     fetch(`https://api.myjson.com/bins/gm49r`)
       .then(res => res.json())
       .then(data => {
         let stockNames = data.stocks.map(x => x.name);
+        let sessionCompany = sessionStorage.getItem("sessionSelectedCompany");
+        let selectedStockData = "";
+        let randomCite = this.randomCite();
+        if (sessionCompany) {
+          selectedStockData = data.stocks.filter(x => x.name === sessionCompany);
+        }
+
         this.setState({
           stockNames,
-          stockData: data
-
+          stockData: data,
+          selectedCompany: sessionCompany ? sessionCompany : "",
+          selectedStockData: selectedStockData ? selectedStockData : "",
+          author: randomCite.author,
+          cite: randomCite.cite,
         });
       });
   }
 
+
+
   handleChange = selectedCompany => {
+    sessionStorage.setItem('sessionSelectedCompany', selectedCompany.value);
     let selectedStockData = this.state.stockData.stocks.filter(x => x.name === selectedCompany.value);
     this.setState({ selectedCompany: selectedCompany.value, selectedStockData });
   };
 
   render() {
-    if (!this.state.selectedCompany && !this.state.selectedStockData) {
+    if (!this.state.stockData) {
+      return (<><Lines /></>);
+    } else {
       let options = this.state.stockNames.map(function (stockName) {
         return { value: stockName, label: stockName };
       })
-      return (<><div className='centerDiv'>
 
-        <div>
-          <Select
-            className='selector'
-            name="companyNameSelector"
-            value={this.state.selectedCompany.value}
-            onChange={this.handleChange}
-            clearable={this.state.clearable}
-            searchable={this.state.searchable}
-            options={options}
-          />
-        </div>
-        <Cites citeNo={this.state.citeNo} />
-      </div>
-      </>);
-    } else {
-      return <Main stock={this.state.selectedCompany} stockData={this.state.selectedStockData} />
+      if (!this.state.selectedCompany && !this.state.selectedStockData) {
+        return (<>
+          <div className="entryImg">
+
+            <div className='centerDiv'>
+              <div>
+                <Select
+                  className='selector'
+                  name="companyNameSelector"
+                  placeholder='Wybierz spółkę'
+                  value={this.state.selectedCompany.value}
+                  onChange={this.handleChange}
+                  clearable={this.state.clearable}
+                  searchable={this.state.searchable}
+                  options={options}
+                />
+              </div>
+              <Cites author={this.state.author} cite={this.state.cite} />
+            </div>
+          </div>
+        </>);
+      } else {
+        debugger;
+        let currOptions = options.filter(item => { return item.value != sessionStorage.getItem('sessionSelectedCompany') })
+        return (<><div className="stick-header"><header><Header stock={this.state.selectedStockData[0]} options={currOptions} handler={this.handler} /></header></div>
+          <Main stock={this.state.selectedCompany} stockData={this.state.selectedStockData} />
+        </>)
+      }
     }
   }
 }
--- dev/js/components/stock.js
+++ dev/js/components/stock.js
@@ -69,8 +69,12 @@ class FinFactor extends Component {
     const currAccVal = (parseFloat(this.props.actualYear.acc_value) * this.props.actualYear.equites);
     const prevAccVal = (parseFloat(this.props.previousYear.acc_value) * this.props.previousYear.equites);
     const lastPrice = this.props.actualYear.price.map(x => x.value)[11];
+    const yearBeforePrice = this.props.previousYear.price.map(x => x.value)[11];
     const priceToProfit = parseFloat(lastPrice / (currAccVal / this.props.actualYear.equites)).toFixed(2);
     const priceToValue = parseFloat(lastPrice / (this.props.actualYear.net_profit / this.props.actualYear.equites)).toFixed(2);
+    const prevPriceToProfit = parseFloat(yearBeforePrice / (prevAccVal / this.props.previousYear.equites)).toFixed(2);
+    const prevPriceToValue = parseFloat(yearBeforePrice / (this.props.previousYear.net_profit / this.props.previousYear.equites)).toFixed(2);
+
     return (<><h2>Wskaźniki finansowe</h2>
       <table>
         <thead>
@@ -89,22 +93,22 @@ class FinFactor extends Component {
           <tr>
             <th>C/Z</th>
             <td>{priceToProfit}</td>
-            <td>{'%'}</td>
+            <td>{Math.abs(priceToProfit - prevPriceToProfit) / prevPriceToProfit * 100 + '%'}</td>
           </tr>
           <tr>
             <th>C/WK</th>
             <td>{priceToValue}</td>
-            <td>{'%'}</td>
+            <td>{Math.abs(priceToValue - prevPriceToValue) / prevPriceToValue * 100 + '%'}</td>
           </tr>
           <tr>
             <th>Wskaźnik W. Buffeta</th>
             <td>{(priceToProfit * priceToValue).toFixed(2) > 20 || (priceToProfit * priceToValue).toFixed(2) < 0 ? <span style={{ color: 'orange' }}>{(priceToProfit * priceToValue).toFixed(2)}</span> : <span style={{ color: 'green' }}>{(priceToProfit * priceToValue).toFixed(2)}</span>}</td>
-            <td>{}</td>
+            <td>{Math.abs((prevPriceToProfit * prevPriceToValue) - (priceToProfit * priceToValue)) / (prevPriceToProfit * prevPriceToValue) * 100 + '%'}</td>
           </tr>
           <tr>
             <th>Liczba akcji</th>
             <td>{this.props.actualYear.equites / 1000} tys. szt.</td>
-            <td>{(this.props.actualYear.equites - this.props.previousYear.equites) / 1000} tys. szt.</td>
+            <td>{(this.props.actualYear.equites - this.props.previousYear.equites) / 1000}tys. szt.</td>
           </tr>
         </tbody>
       </table>
@@ -116,7 +120,6 @@ class FinFactor extends Component {
 class Shares extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
@@ -136,7 +139,7 @@ class Shares extends Component {
           ]
         }]
     }
-    return (<div><h2>Akcjonariat</h2><Doughnut width={100} options={{ maintainAspectRatio: false }} data={chartData} /></div>
+    return (<div><h2>Akcjonariat</h2><Doughnut options={{ maintainAspectRatio: true }} data={chartData} /></div>
     )
   }
 }
@@ -239,19 +242,22 @@ class LineGraph extends Component {
   }
 }
 
+function jsUcfirst(paragraph) {
+  debugger;
+  let firstWord = paragraph[0].split(' ')[0];
+  return firstWord.charAt(0).toUpperCase() + paragraph[0].slice(1);
+}
+
 
 class StockInfo extends Component {
   constructor(props) {
     super(props);
   }
-
-
   render() {
-    debugger;
     return (<>
       <table>
         <tbody>
-          {this.props.info.map((event, index) => <tr key={index}><td>{event}</td></tr>)}
+          {this.props.info.map((event, index) => <tr key={index}><td><p>{jsUcfirst(event)}</p></td></tr>)}
         </tbody>
       </table>
     </>)
